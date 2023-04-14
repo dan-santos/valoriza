60b674bc870c61431b28a08bd9ac5f5ff8b025dd
Dan <santos.danielfd@gmail.com>
2023-04-13 23:33:14 -0300

    updated usureAdmin middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
-import { UnauthorizedError } from '../utils/CustomErrors';
+import { UsersRepository } from '../repositories/User/UsersRepository';
+import { FindUserService } from '../services/User/FindUserService';
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-  const admin = true;
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+  const { user_id } = req;
 
-  if (admin) {
-    return next();
-  } 
-  throw new UnauthorizedError('Unauthorized User');
+  if(!user_id) return res.status(400).json('Missing "userId" attribute');
+
+  const usersRepository = new UsersRepository();
+  const findUserService = new FindUserService(usersRepository);
+
+  const { admin } = await findUserService.execute({ id: user_id });
+
+  if (!admin) return res.status(403).json('User not enough privileges to create a tag');
+
+  return next();
 }
