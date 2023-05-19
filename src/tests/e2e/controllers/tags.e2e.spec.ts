f9e965aebe2beff7392986f2482a9770f7a01983
Dan <santos.danielfd@gmail.com>
2023-05-19 00:06:22 -0300

    finished tag routes e2e tests

--- src/tests/e2e/controllers/tags.e2e.spec.ts
+++ src/tests/e2e/controllers/tags.e2e.spec.ts
@@ -8,8 +8,10 @@ import { Tag } from '../../../entities/Tag';
 describe('[E2E] Testing all tags related endpoints', async () => {
 
   const baseURL = '/tags';
+  const successStatusCode = 200;
   const { token } = await getAdminTokenAndId(new UsersRepository);
-  const payload: Partial<Tag> = { name: 'AnyTagName' };
+  const payload: Partial<Tag> = { name: 'CreatedTag' };
+  let createdTag: Tag;
 
   it('CREATE', async () => {
     const response = await request(app)
@@ -17,6 +19,36 @@ describe('[E2E] Testing all tags related endpoints', async () => {
       .set('Authorization', `Bearer ${token}`)
       .send(payload);
 
-    expect(response.statusCode).toStrictEqual(200);
+    createdTag = response.body as Tag;
+    expect(response.statusCode).toStrictEqual(successStatusCode);
+  });
+
+  it('GET', async () => {
+    const response = await request(app)
+      .get(baseURL)
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toStrictEqual(successStatusCode);
+  });
+
+  it('PUT', async () => {
+    createdTag.name = 'UpdatedTag';
+
+    const response = await request(app)
+      .put(`${baseURL}/${createdTag.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(createdTag);
+
+    expect(response.statusCode).toStrictEqual(successStatusCode);
+  });
+
+  it('DELETE', async () => {
+    const response = await request(app)
+      .delete(`${baseURL}/${createdTag.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toStrictEqual(successStatusCode);
   });
 });
